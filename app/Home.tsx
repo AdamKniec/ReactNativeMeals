308a3c0e96f49b0eb602c36a37cca43b595ab50a
AdamKniec <adam.k.kniec@gmail.com>
2026-02-05 19:47:02 +0100

    move styles out of JSX

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,16 +1,10 @@
 import MealItemGridType from "@/components/MealTypeGridItem/MealTypeGridItem";
 import { SAMPLE_DATA } from "@/constants/dummyData";
-import { FlatList, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={Styles.wrapper}>
       <FlatList
         data={SAMPLE_DATA}
         numColumns={2}
@@ -25,3 +19,11 @@ export default function Index() {
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
